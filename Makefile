91440e61549c91466c80eceb34601e94418d2948
Uros Mrkobrada <uros.mrkobrada@gmail.com>
2024-05-31 17:15:02 +0200

    updated extension.ts

--- Makefile
+++ Makefile
@@ -3,6 +3,5 @@ build:
 	npm run build2
 	cd editors/code && npm install && npm run compile3 && npm run package
 
-
-	
-	
+package:
+	cd editors/code && npm run compile3 && npm run package
--- editors/code/src/extension.ts
+++ editors/code/src/extension.ts
@@ -5,7 +5,6 @@
 
 import * as path from 'path';
 import { workspace, ExtensionContext } from 'vscode';
-import { platform } from 'os';
 
 import {
 	LanguageClient,
@@ -15,8 +14,6 @@ import {
 } from 'vscode-languageclient/node';
 
 import * as vscode from 'vscode';
-import { existsSync,  writeFileSync } from 'fs';
-import {  spawnSync } from 'child_process';
 
 // const binding = require('node-gyp-build')('node_modules/asyncflows-lsp/node_modules/@tree-sitter-grammars/tree-sitter-yaml');
 
@@ -24,18 +21,17 @@ let client: LanguageClient;
 
 export async function activate(context: ExtensionContext) {
 	let output = vscode.window.createOutputChannel("dbg-asyncflows-client");
-	const reading = await renameTreeSitterPath(context.extensionPath, output);
 	// The server is implemented in node
 	// const serverModule = context.asAbsolutePath('../../bin/asyncflows-lsp');	
 	const serverModule = path.join(context.extensionPath, 'dist', 'languageserver.js');
 	let config = {};
 	let debugOptions = { execArgv: ['--nolazy', '--inspect=6009'] };
-
+	const tsPath = tsPaths(context.extensionPath);
 
 	// If the extension is launched in debug mode then the debug server options are used
 	// Otherwise the run options are used
 	const serverOptions: ServerOptions = {
-		run: { module: serverModule, transport: TransportKind.ipc },
+		run: { module: serverModule, transport: TransportKind.ipc, options: { env: { 'YAML_PATH': tsPath[0], 'TREESITTER_PATH': tsPath[1] } } },
 		debug: {
 			module: serverModule,
 			transport: TransportKind.ipc,
@@ -107,13 +103,7 @@ export function deactivate(): Thenable<void> | undefined {
 	return client.stop();
 }
 
-export async function renameTreeSitterPath(extensionPath: string, output: vscode.OutputChannel): Promise<boolean> {
-	const tempFile = path.join(extensionPath, 'renamed.txt');
-	let resolving;
-	const promise = new Promise<boolean>((resolve) => resolving = resolve);
-	if (existsSync(tempFile)) {
-		return Promise.resolve(true);
-	}
+function tsPaths(extensionPath: string): [string, string] {
 
 	const yamlGrammar = "node_modules/@tree-sitter-grammars/tree-sitter-yaml/bindings/node";
 	const treeSitter = "node_modules/tree-sitter";
@@ -121,35 +111,6 @@ export async function renameTreeSitterPath(extensionPath: string, output: vscode
 	const newYamlPath = path.join(extensionPath, yamlGrammar);
 	const newTreeSitterPath = path.join(extensionPath, treeSitter);
 
-	const pathLs = path.join(extensionPath, 'dist', 'languageserver.js');
+	return [newYamlPath, newTreeSitterPath]
 
-	function rename() {
-		const platformName = platform();
-		if (platformName == 'linux' || platformName == 'darwin') {
-			spawnSync('sed', ["-i''", '-e', `'s|${yamlGrammar}|${newYamlPath}|'`, pathLs]);
-			spawnSync('sed', ["-i''", '-e', `'s|${treeSitter}|${newTreeSitterPath}|'`, pathLs]);
-			writeFileSync(tempFile, 'true');
-		}
-		else if (platformName == 'win32') {
-			const windowsCommand = `
-    $content = Get-Content -Path "${pathLs}"; 
-    $updatedContent = $content -replace "${yamlGrammar}", "${newYamlPath}"; 
-    $updatedContent = $updatedContent -replace "${treeSitter}", "${newTreeSitterPath}"; 
-    Set-Content -Path "${pathLs}" -Value $updatedContent
-`;
-			spawnSync('powershell.exe', ['-Command', windowsCommand]);
-			writeFileSync(tempFile, 'true');
-		}
-	}
-
-	try {
-		rename();
-		resolving(true);
-	}
-
-	catch (e) {
-		output.appendLine('Error while updating language-server.js')
-	}
-
-	return promise;
 }
