2ee3b208b75765745d2676fec3e869f778fa737c
Uros Mrkobrada <uros.mrkobrada@gmail.com>
2024-06-18 11:59:31 +0200

    updated settingsHandlers.ts

--- src/languageserver/handlers/settingsHandlers.ts
+++ src/languageserver/handlers/settingsHandlers.ts
@@ -403,9 +403,12 @@ export class SettingsHandler {
           if(!l.description) {
             l.description = ""
           }
+          if(!l.markdownDescription) {
+            l.markdownDescription = "";
+          }
           const enumValues = l.enum;
           for (const e of enumValues) {
-            links.push({name: e, description: l.description});
+            links.push({name: e, description: l.markdownDescription});
           }
         }
         this.languageService.globalJinjaActions.set(uri, links)
